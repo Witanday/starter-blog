f6d44c67e78718b1a79c0a6685813f4e2b75e59d
Witanday <witanday.cd@gmail.com>
2019-03-27 07:14:49 -0400

    initial starter commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `WitandayDev Blog`,
+    author: `Witanday Kyanga`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      facebook: `Witanday`,
     },
   },
   plugins: [
--- src/components/bio.js
+++ src/components/bio.js
@@ -38,11 +38,11 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong> who lives and works in Boston-Massachussets building useful things.
               {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
+              <a //href={`https://www.facebook.com/${social.facebook}`
+            href='https://www.facebook.com/witanday'>
+                You should follow him on Facebook
               </a>
             </p>
           </div>
